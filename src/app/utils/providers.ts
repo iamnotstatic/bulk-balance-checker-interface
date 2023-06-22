ad9642cd1156db108161dd278cd8b688ce2999cc
iamnotstatic <staticdev20046@gmail.com>
2023-06-22 21:01:07 +0100

    Update ethers provider URL

--- src/app/utils/providers.ts
+++ src/app/utils/providers.ts
@@ -1,5 +1,3 @@
 import * as Ethers from 'ethers';
 
-export const ethers = new Ethers.JsonRpcProvider(
-  'https://mainnet.infura.io/v3/b509a32f1d2a412d958d135660d62b8a'
-);
+export const ethers = new Ethers.JsonRpcProvider('https://eth.llamarpc.com');
