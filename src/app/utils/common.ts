0f10e55aab538579b4286cec47b06de1bceab9b4
iamnotstatic <staticdev20046@gmail.com>
2023-06-24 00:47:42 +0100

    Add Ethereum and BSC contract addresses and RPC URLs

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -10,6 +10,15 @@ export type AddressBalanceMap = {
   [address: string]: BalanceMap;
 };
 
+export const ETH_CONTRACT_ADDRESS =
+  '0xb1f8e55c7f64d203c1400b9d8555d050f94adf39';
+export const BSC_CONTRACT_ADDRESS =
+  '0x2352c63A83f9Fd126af8676146721Fa00924d7e4';
+
+export const ETH_RPC_URL = 'https://eth.llamarpc.com';
+export const BSC_RPC_URL =
+  'https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3';
+
 export function formatAddressBalances<T extends { toString: () => string }>(
   values: T[],
   addresses: string[],
