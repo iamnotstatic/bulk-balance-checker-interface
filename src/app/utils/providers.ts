7c0f8e467a9d4ad162775fe4e6b8395cfdd103c2
iamnotstatic <staticdev20046@gmail.com>
2023-06-23 17:16:22 +0100

    Add Binance Smart Chain provider and contract

--- src/app/utils/providers.ts
+++ src/app/utils/providers.ts
@@ -1,3 +1,14 @@
 import * as Ethers from 'ethers';
+import BalanceCheckerABI from '../abis/BalanceChecker.abi.json';
 
-export const ethers = new Ethers.JsonRpcProvider('https://eth.llamarpc.com');
+const provider = new Ethers.JsonRpcProvider(
+  'https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3'
+);
+
+const contract = new Ethers.Contract(
+  '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
+  BalanceCheckerABI,
+  provider
+);
+
+export { contract, provider };
