0640e481ab5fa0c6003232a851318f687456a214
iamnotstatic <staticdev20046@gmail.com>
2023-06-24 00:47:22 +0100

    Refactor ethers provider and contract creation

--- src/app/utils/providers.ts
+++ src/app/utils/providers.ts
@@ -1,14 +1,9 @@
 import * as Ethers from 'ethers';
 import BalanceCheckerABI from '../abis/BalanceChecker.abi.json';
 
-const provider = new Ethers.JsonRpcProvider(
-  'https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3'
-);
+const provider = (network: string) => new Ethers.JsonRpcProvider(network);
 
-const contract = new Ethers.Contract(
-  '0x2352c63A83f9Fd126af8676146721Fa00924d7e4',
-  BalanceCheckerABI,
-  provider
-);
+const contract = (network: string, address: string) =>
+  new Ethers.Contract(address, BalanceCheckerABI, provider(network));
 
 export { contract, provider };
