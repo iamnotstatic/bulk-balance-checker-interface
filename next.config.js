e3c76f1968371926368758a1d042c7dbdec64df6
iamnotstatic <staticdev20046@gmail.com>
2023-07-18 16:38:51 +0100

    Add remote image patterns to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+        protocol: 'https',
+        hostname: 'cryptologos.cc',
+        port: '',
+        pathname: '/logos/**',
+    }],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
