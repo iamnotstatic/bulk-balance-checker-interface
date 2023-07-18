09a692d428063fec2855b0f50d31ea32f2b4547c
iamnotstatic <staticdev20046@gmail.com>
2023-07-18 16:39:59 +0100

    Update default asset addresses and logos for ETH and BSC networks

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -8,7 +8,7 @@ export type Asset = {
 
 export const ethDefaultAssets: Asset[] = [
   {
-    address: ethers.ZeroAddress,
+    address: '0x0000000000000000000000000000000000000000',
     symbol: 'ETH',
     logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=014',
   },
@@ -30,23 +30,18 @@ export const ethDefaultAssets: Asset[] = [
   {
     address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
     symbol: 'DAI',
-    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.svg?v=014',
+    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=014',
   },
   {
-    address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
-    symbol: 'WBTC',
-    logo: 'https://cryptologos.cc/logos/wrapped-bitcoin-wbtc-logo.png?v=014',
-  },
-  {
-    address: '0x',
-    symbol: 'N/A',
-    logo: 'https://user-images.githubusercontent.com/46509072/195873643-471eaa95-cb32-4675-a892-329a66cb0ee1.png',
+    address: '0x4Fabb145d64652a948d72533023f6E7A623C7C53',
+    symbol: 'BUSD',
+    logo: 'https://cryptologos.cc/logos/binance-usd-busd-logo.png?v=014',
   },
 ];
 
 export const bscDefaultAssets: Asset[] = [
   {
-    address: ethers.ZeroAddress,
+    address: '0x0000000000000000000000000000000000000000',
     symbol: 'BNB',
     logo: 'https://cryptologos.cc/logos/binance-coin-bnb-logo.png?v=014',
   },
@@ -54,11 +49,6 @@ export const bscDefaultAssets: Asset[] = [
     address: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     symbol: 'WBNB',
     logo: 'https://cryptologos.cc/logos/binance-coin-bnb-logo.png?v=014',
-  },
-  {
-    address: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
-    symbol: 'BUSD',
-    logo: 'https://cryptologos.cc/logos/binance-usd-busd-logo.png?v=014',
   },
   {
     address: '0x55d398326f99059fF775485246999027B3197955',
@@ -73,11 +63,11 @@ export const bscDefaultAssets: Asset[] = [
   {
     address: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
     symbol: 'DAI',
-    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.svg?v=014',
+    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=014',
   },
   {
-    address: '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
-    symbol: 'Cake',
-    logo: 'https://cryptologos.cc/logos/pancakeswap-cake-logo.svg?v=14',
+    address: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+    symbol: 'BUSD',
+    logo: 'https://cryptologos.cc/logos/binance-usd-busd-logo.png?v=014',
   },
 ];
