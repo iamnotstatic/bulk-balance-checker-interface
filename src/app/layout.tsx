17e80139541e96fed9d93b724512520a42b401df
iamnotstatic <staticdev20046@gmail.com>
2023-07-20 01:48:43 +0100

    Update metadata title and description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Blockchain Bulk balance Checker',
-  description: 'Check the balance of multiple blockchain addresses at once',
+  title: 'Bulk balance Checker',
+  description:
+    'Discover the Collective Balance of Multiple Addresses and Assets Instantly!',
+  keywords:
+    'ethereum, bnb smart chain, polygon, arbitrum, blockchain, bulk, balance, checker, erc20, tokens, assets, addresses, web3, wallets, cryptocurrencies, defi, dapps, decentralized applications, smart contract, smart contracts, solidity',
 };
 
 export default function RootLayout({
