62ed61ffeb6775a87de0bb4c93c7c36762dfe623
iamnotstatic <staticdev20046@gmail.com>
2023-07-24 18:01:13 +0300

    Update main element style in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Balance from './balance';
 
 const Home = async () => {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-5 lg:p-24">
       <Balance />
     </main>
   );
